6f28ef6b65bc66e394cbf8dbf7bc4dd277eb0366
Thomas Dohmke <thomas@dohmke.de>
2011-06-29 16:58:20 +0200

    Added license header.

--- Classes/CNSApp.m
+++ Classes/CNSApp.m
@@ -1,3 +1,23 @@
+// Copyright 2011 Codenauts UG. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #import "CNSApp.h"
 #import "CNSConnectionHelper.h"
 #import "CNSPreferencesViewController.h"
@@ -168,7 +188,7 @@
   [request setHTTPBody:body];
   
   self.connectionHelper = [[CNSConnectionHelper alloc] initWithRequest:request delegate:self selector:@selector(parseVersionResponse:) identifier:nil];
-  self.progressIndicator.hidden = NO;
+  [self.progressIndicator setHidden:NO];
 }
 
 #pragma mark - CNSConnectionHelper Delegate Methods
@@ -194,7 +214,7 @@
   
   self.statusLabel.stringValue = errorMessage;
   self.progressIndicator.doubleValue = 0;
-  self.progressIndicator.hidden = YES;
+  [self.progressIndicator setHidden:YES];
   [self.cancelButton setTitle:@"Done"];
   
   [pool drain];
--- Classes/CNSArchive.m
+++ Classes/CNSArchive.m
@@ -1,3 +1,23 @@
+// Copyright 2011 Codenauts UG. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
 #import "CNSArchive.h"
 #import "CNSConnectionHelper.h"
 #import "CNSPreferencesViewController.h"
@@ -93,6 +113,7 @@
   [request setHTTPBody:body];
   
   self.connectionHelper = [[CNSConnectionHelper alloc] initWithRequest:request delegate:self selector:@selector(parseVersionResponse:) identifier:nil];
+  [self.progressIndicator setHidden:NO];
 }
 
 - (NSString *)createIPAFromFileWrapper:(NSFileWrapper *)fileWrapper {
